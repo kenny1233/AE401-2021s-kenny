9f93a44b4799ad445689afeba4b06585663182a7
kenny1233 <80561843+kenny1233@users.noreply.github.com>
2021-05-29 12:12:40 +0800

    Add files via upload

---
+++ 09.py
@@ -0,0 +1,23 @@
+from mcpi.minecraft import Minecraft
+mc=Minecraft.create
+import time
+
+def buildPyramid(x,y,z):
+    base=10
+    height=base//2+1
+    
+    for i in range(height):
+        x1=x+i
+        y1=y+i
+        z1=z+i
+        
+        x2=x+base-i
+        z2=z+base-i
+        
+        mc.setBlocks(x1,y1,z1,x2,y1,z2,20)
+        
+        
+x,y,z=mc.player.getTilePos()
+
+buildPyramid(x,y,z)
+        
